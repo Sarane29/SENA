6a9d1aa85bffb6b44692038d0b5b5957a669d62e
Sarane29 <saranicolencisocarrillo29@gmail.com>
2025-10-24 11:45:17 -0500

    actualizado 3

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/SENA/', // importante: debe tener slash inicial y final
-})
-
+  base: './' // <- rutas relativas para que CSS/JS carguen bien en Netlify
+});
